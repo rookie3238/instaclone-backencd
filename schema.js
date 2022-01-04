785f7f2cbe167916a9adf46af5c9db48603bb9c4
Jang Jung Sik <ice3238@gmail.com>
2022-01-04 22:53:05 +0900

    JangSvrCommit

--- schema.js
+++ schema.js
@@ -6,8 +6,9 @@ import {
 } from "graphql-tools";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-
+//Graphql Type 정의시 하위 디렉토리에서 뒤의 확장자가 typeDef.js로 끝나는 것들을 참조
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
+//Graphql Resolver 정의시 하위 디렉토리에서 뒤의 확장자가 resolver.js로 끝나는 것 들을 참조
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
 
