860847acaa464f232dcbb26f71539e8e2fb2abba
JangJungSik <ice3238@gmail.com>
2022-01-04 22:28:35 +0900

    220104 커밋

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ require("dotenv").config();
 
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const server = new ApolloServer({
   schema,
+
+  //사용자의 프로파일을 수정할때 Muation에 일일이 토큰을 넘겨 주는 것은 비효율적
+  //http header에 토근을 실어 넘겨 준다
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+    };
+  },
 });
 const PORT = process.env.PORT;
 server
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,42 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../../client";
+//import { protectedProfile } from "";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("안녕"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword },
+      { loggedInUser }
+    ) => {
+      console.log(loggedInUser);
+
+      let uglyPassword = null;
+
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id: loggedInUser.id,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+
+      if (updatedUser.id) {
+      } else {
+        return {
+          ok: false,
+          error: "Profile을 수정 할 수 없습니다",
+        };
+      }
+    },
   },
 };
